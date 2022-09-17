134e8e79c12b32726fde5a59bf98af68ced10622
0kim <ceo.youngkim@gmail.com>
2022-09-17 15:03:09 +0900

    캐릭터 채팅 조작

--- dist/main.js
+++ dist/main.js
@@ -1,15 +1,31 @@
 /**
  * Copyright (c) 2022 ZEP Co., LTD
  */
-const spartan = App.loadSpritesheet('spartan.png', 64, 96, {
-  left: [0, 1, 2, 3],
-  up: [0],
-  down: [0],
-  right: [0, 1, 2, 3]
-}, 8);
+
+/* 스파르탄 캐릭터 조작 */
+// const spartan = ScriptApp.loadSpritesheet('spartan.png',64,96, {
+//   left: [0,1,2,3],
+//   up: [0],
+//   down: [0],
+//   right: [0,1,2,3]
+// }, 8);
+
+/* 채팅 조작 */
+App.onSay.Add(function (player, text) {
+  if (text == '스피드업') {
+    player.moveSpeed = 400;
+    player.sendUpdated();
+  } else if (text == '스피드다운') {
+    player.moveSpeed = 40;
+    player.sendUpdated();
+  } else {
+    const message = player.name + '님이 \'' + text + '\' 라고 말했습니다';
+    App.showCenterLabel(message);
+  }
+});
 App.onJoinPlayer.Add(function (player) {
-  App.showCenterLabel("Hello World");
-  player.sprite = spartan;
+  App.showCenterLabel("Hello World"); // player.sprite = spartan;
+
   player.title = '김영완두콩';
   player.moveSpeed = 300;
   player.sendUpdated();
--- main.ts
+++ main.ts
@@ -4,16 +4,31 @@
 
 import "zep-script";
 
-const spartan = ScriptApp.loadSpritesheet('spartan.png',64,96, {
-  left: [0,1,2,3],
-  up: [0],
-  down: [0],
-  right: [0,1,2,3]
-}, 8);
+/* 스파르탄 캐릭터 조작 */
+// const spartan = ScriptApp.loadSpritesheet('spartan.png',64,96, {
+//   left: [0,1,2,3],
+//   up: [0],
+//   down: [0],
+//   right: [0,1,2,3]
+// }, 8);
+
+/* 채팅 조작 */
+ScriptApp.onSay.Add(function(player, text) {
+  if(text == '스피드업') {
+    player.moveSpeed = 400;
+    player.sendUpdated();
+  } else if(text == '스피드다운') {
+    player.moveSpeed = 40;
+    player.sendUpdated();
+  } else {
+    const message = player.name + '님이 \'' + text + '\' 라고 말했습니다';
+    ScriptApp.showCenterLabel(message);
+  }
+})
 
 ScriptApp.onJoinPlayer.Add(function(player) {
   ScriptApp.showCenterLabel("Hello World");
-  player.sprite = spartan;
+  // player.sprite = spartan;
   player.title = '김영완두콩';
   player.moveSpeed = 300;
   player.sendUpdated();
