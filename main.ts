72f4c1de2dc59ef23eade27800967d52e9968540
young_kim <youngkim9012@gmail.com>
2022-09-21 00:13:57 +0900

    플레이어 이벤트 핸들러

--- main.ts
+++ main.ts
@@ -26,6 +26,40 @@ ScriptApp.onSay.Add(function(player, text) {
   }
 })
 
+/* 플레이어 충돌 */
+ScriptApp.onPlayerTouched.Add(function(sender, target, x, y){
+// 플레이어끼리 부딪힐 때 실행
+  ScriptApp.showCenterLabel(
+		`${sender.name}와 ${target.name}가 좌표 ${x}, ${y} 에서 만나부러쓰`
+	);
+});
+
+/* 오브젝트 충돌 */
+ScriptApp.onObjectTouched.Add(function (sender, x, y, tileID) {
+  ScriptMap.putObject(x, y, null);
+  ScriptApp.showCenterLabel(
+      `${sender.name}가 좌표 ${x}, ${y} 에서 오브젝트와 부딪쳐부러쓰`
+  );
+});
+
+/* 플레이어 공격 */
+ScriptApp.onUnitAttacked.Add(function (sender, x, y, target) {
+  ScriptApp.showCenterLabel(`${sender.name}가 ${target.name}를 공격해부러쓰`);
+  ScriptApp.sayToAll(`(${x}, ${y})`);
+});
+
+/* 오브젝트 공격 */
+ScriptApp.onObjectAttacked.Add(function(sender, x, y){
+  ScriptApp.showCenterLabel(
+      `${sender.name}가 좌표 ${x}, ${y} 에서 오브젝트를 공격해부러쓰`
+  );
+})
+
+/* 오브젝트 공격 */
+ScriptApp.onSidebarTouched.Add(function (player) {
+  ScriptApp.sayToAll(`${player.name}님이 사이드바 를 눌러부러쓰`)
+});
+
 const cloud = ScriptApp.loadSpritesheet("cloud.png", 659, 400, [0], 6);
 
 ScriptApp.onJoinPlayer.Add(function(player) {
